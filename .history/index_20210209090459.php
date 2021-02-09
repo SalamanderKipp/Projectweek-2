db1085c2936d3465380508d52acaed90677a18fc
Maarten <75844243+SalamanderKipp@users.noreply.github.com>
2021-02-09 09:18:26 +0100

    Initial commit

---
+++ .history/index_20210209090459.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project periode 2</title>
+    <link rel="stylesheet" href="assest/css/style.css">
+</head>
+<body>
+<!-- container -->
+<div class="container">
+<div class="card-">
+Card
+
+</div>
+</div>
+<!-- card design in blok -->
+</body>
+</html>
