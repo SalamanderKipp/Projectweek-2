d85d27515e139011d83dc2ce43f1dc8aa97bcf85
Maarten <75844243+SalamanderKipp@users.noreply.github.com>
2021-02-19 12:08:48 +0100

    fixed

--- detail.php
+++ detail.php
@@ -13,7 +13,6 @@
         <title>Detail</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href='assets/css/style.css' rel='stylesheet'>
-        <link href='assets/css/detail.css' rel='stylesheet'>
     </head>
 
     <body>
